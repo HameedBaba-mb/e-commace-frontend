fec0d77a4bd138c0a9e5c2291204aed67eaab0cf
alkali abdulhamid <alkali.abdulhamid@itcglobal.ng>
2025-04-30 14:41:36 +0100

    logout

--- src/components/client/ClientNavbar.vue
+++ src/components/client/ClientNavbar.vue
@@ -53,62 +53,19 @@
             </li>
           </ul>
           <div class="navbar-collapse justify-content-end px-0" id="navbarNav">
-        <ul
-          class="navbar-nav flex-row ms-auto align-items-center justify-content-end"
-        >
-          <li class="nav-item dropdown">
-            <a
-              class="nav-link nav-icon-hover"
-              href="javascript:void(0)"
-              id="drop2"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
+            <ul
+              class="navbar-nav flex-row ms-auto align-items-center justify-content-end"
             >
-              <img
-                src="../../assets/images/profile/user-1.jpg"
-                alt=""
-                width="35"
-                height="35"
-                class="rounded-circle"
-              />
-            </a>
-            <div
-              class="dropdown-menu dropdown-menu-end dropdown-menu-animate-up"
-              aria-labelledby="drop2"
-            >
-              <div class="message-body">
-                <a
-                  href="javascript:void(0)"
-                  class="d-flex align-items-center gap-2 dropdown-item"
-                >
-                  <i class="ti ti-user fs-6"></i>
-                  <p class="mb-0 fs-3">Change Passsword</p>
-                </a>
-                <a
-                  href="javascript:void(0)"
-                  class="d-flex align-items-center gap-2 dropdown-item"
-                >
-                  <i class="ti ti-mail fs-6"></i>
-                  <p class="mb-0 fs-3">Delete Account</p>
-                </a>
-                <!-- <a
-                  href="javascript:void(0)"
-                  class="d-flex align-items-center gap-2 dropdown-item"
-                >
-                  <i class="ti ti-list-check fs-6"></i>
-                  <p class="mb-0 fs-3">My Task</p>
-                </a> -->
+              <li class="nav-item dropdown">
                 <router-link
                   :to="{ name: 'login' }"
-                  class="btn btn-outline-primary mx-3 mt-2 d-block"
-                  >Logout</router-link
+                  class="btn btn-outline-light mx-3 mt-2 d-block"
                 >
-              </div>
-            </div>
-          </li>
-        </ul>
-      </div>
-
+                  <i class="fa fa-sign-out me-2"></i> Logout</router-link
+                >
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
